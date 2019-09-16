0dd1d315956211155c9c34ae041b8e1d104a1deb
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 12:41:44 +0530

    Update server.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -15,7 +15,7 @@ module.exports.run = (config) => {
 
 
 
-
+const pg = require('pg');
 
 var connectionString = "postgres://bzshbrrnoxietj:46501dac4a137a60da10a1de5fd5741ddc665c4337c36ea5007458c5c46dc1bf@ec2-174-129-194-188.compute-1.amazonaws.com:5432/d3cld8si9il4oe"
 
