29f86c8df28a4484569f1e14c84307b7a26704a3
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 14:32:09 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -7,4 +7,23 @@ server.run(config);
 
 
 
+var express    = require("express");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'address_book'
+});
+var app = express();
+
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");    
+} else {
+    console.log("Error connecting database ... nn");    
+}
+});
+
+
 
