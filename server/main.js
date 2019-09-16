d03d76bcaff9a6d6468b74ba83df85c43e104953
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 14:34:21 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -10,10 +10,10 @@ server.run(config);
 var express    = require("express");
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'address_book'
+  host     : 'sql12.freemysqlhosting.net',
+    database : 'sql12304794',
+    user     : 'sql12304794',
+    password : 'PLSEEGHnWv'
 });
 var app = express();
 
