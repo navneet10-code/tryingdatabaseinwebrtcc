0a0dd91b7771b1e948aec52798f5f54e9b90fd69
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 13:12:52 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -5,20 +5,6 @@ config.PORT = process.env.PORT || config.PORT;
 
 server.run(config);
 
-var mysql = require('mysql');  
-var con = mysql.createConnection({  
-  host     : 'sql12.freemysqlhosting.net',
-    database : 'sql12304794',
-    user     : 'sql12304794',
-    password : 'PLSEEGHnWv', 
-});  
-con.connect(function(err) {  
-if (err) throw err;  
-con.query("SELECT * FROM user", function (err, result) {  
-if (err) throw err;  
-console.log(result);  
-});  
-});  
 
 
 
