18380bb9b0371bdd416ae70737a0d1867fa67983
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 18:42:59 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -59,6 +59,14 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 
+app.get('/', function(req, res){
+  res.render('form');// if jade
+  // You should use one of line depending on type of frontend you are with
+  //res.sendFile(__dirname + '/form.html'); //if html file is root directory
+ res.sendFile("index.html"); //if html file is within public directory
+});
+
+
 app.post('/login', urlencodedParser, function (req, res) {
   console.log(req.body);
   res.send('welcome, ' + req.body.username)
