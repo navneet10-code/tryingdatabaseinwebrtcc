c35e744c6bd0da9b768e30457281a51c1b262bfb
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 12:00:42 +0530

    Update web.js

--- server/lib/web.js
+++ server/lib/web.js
@@ -1,9 +1,16 @@
-var connectionString = "postgres://bzshbrrnoxietj:46501dac4a137a60da10a1de5fd5741ddc665c4337c36ea5007458c5c46dc1bf@ec2-174-129-194-188.compute-1.amazonaws.com:5432/d3cld8si9il4oe"
+const { Client } = require('pg');
 
-pg.connect(connectionString, function(err, client, done) {
-   client.query('SELECT * FROM user', function(err, result) {
-      done();
-      if(err) return console.error(err);
-      console.log(result.rows);
-   });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT * FROM user;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
