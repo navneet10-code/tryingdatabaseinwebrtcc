69abaa90b491715d79fb8e6b1455718bbc3eecff
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 17:37:18 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -53,10 +53,10 @@ console.log('nnnnnnnnnnnnnnnnnnnnnnn',someVar);
 
 const express = require('express');
 //const session = require('express-session');
-const bodyParser = require('body-parser');
-const redis = require('redis');
-const redisStore = require('connect-redis')(session);
-const client  = redis.createClient();
+//const bodyParser = require('body-parser');
+//const redis = require('redis');
+//const redisStore = require('connect-redis')(session);
+//const client  = redis.createClient();
 const router = express.Router();
 const app = express();
 
