a68fcc98dc36c67dea2ba48306bd02d6126fdbe3
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 17:00:38 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -7,23 +7,5 @@ server.run(config);
 
 
 
-var express    = require("express");
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'sql12.freemysqlhosting.net',
-    database : 'sql12304794',
-    user     : 'sql12304794',
-    password : 'PLSEEGHnWv'
-});
-var app = express();
-
-connection.connect(function(err){
-if(!err) {
-    console.log("Database is connected ... nn");    
-} else {
-    console.log("Error connecting database ... nn");    
-}
-});
-
 
 
