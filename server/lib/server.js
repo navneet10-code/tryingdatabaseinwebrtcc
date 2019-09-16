032b97bef510509d27956397f21b6f2fa584cd60
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 12:13:46 +0530

    Update server.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -12,3 +12,23 @@ module.exports.run = (config) => {
   socket(server);
   console.log(`Server is listening at :${config.PORT}`);
 };
+
+
+
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT * from salesforce.contact;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
