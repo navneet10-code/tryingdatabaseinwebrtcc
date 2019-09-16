4cd8fc251e7b6d037da8dddde2e8601e35b49bf8
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 13:13:10 +0530

    Update server.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -14,3 +14,20 @@ module.exports.run = (config) => {
 };
 
 
+var mysql = require('mysql');  
+var con = mysql.createConnection({  
+  host     : 'sql12.freemysqlhosting.net',
+    database : 'sql12304794',
+    user     : 'sql12304794',
+    password : 'PLSEEGHnWv', 
+});  
+con.connect(function(err) {  
+if (err) throw err;  
+con.query("SELECT * FROM user", function (err, result) {  
+if (err) throw err;  
+console.log(result);  
+});  
+});  
+
+
+
