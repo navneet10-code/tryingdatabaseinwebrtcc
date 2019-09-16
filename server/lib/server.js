1b87dd1d5fab97406bb5f20d4bc8f2057f24cb8f
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 13:21:01 +0530

    Update server.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -14,7 +14,7 @@ module.exports.run = (config) => {
 };
 
 
-var mysql = require('mysql');  
+/*var mysql = require('mysql');  
 var con = mysql.createConnection({  
   host     : 'sql12.freemysqlhosting.net',
     database : 'sql12304794',
@@ -27,7 +27,36 @@ con.query("SELECT * FROM user", function (err, result) {
 if (err) throw err;  
 console.log(result);  
 });  
-});  
+});  */
+
+
+//var express    = require("express");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'address_book'
+});
+//var app = express();
+
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");    
+} else {
+    console.log("Error connecting database ... nn");    
+}
+});
+
+app.get("/",function(req,res){
+connection.query('SELECT * from user', function(err, rows, fields) {
+connection.end();
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+  });
+});
 
 
 
