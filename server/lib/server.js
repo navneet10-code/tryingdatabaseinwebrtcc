ee0d9eb99291f84ee06cd6a088ace26a47a45759
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-16 13:40:15 +0530

    Update server.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -14,7 +14,7 @@ module.exports.run = (config) => {
 };
 
 
-/*var mysql = require('mysql');  
+var mysql = require('mysql');  
 var con = mysql.createConnection({  
   host     : 'sql12.freemysqlhosting.net',
     database : 'sql12304794',
@@ -27,34 +27,7 @@ con.query("SELECT * FROM user", function (err, result) {
 if (err) throw err;  
 console.log(result);  
 });  
-});  */
+});  
 
-
-const mysql = require('mysql');
-
-const pool = mysql.createPool({
-    connectionLimit : 100, //important
-    host     : 'sql12.freemysqlhosting.net',
-    database : 'sql12304794',
-    user     : 'sql12304794',
-    password : 'PLSEEGHnWv', 
-    debug    :  false
-});
-
-// query rows in the table
-
-function queryRow(userName) {
-    let selectQuery = 'SELECT * FROM user';    
-    let query = mysql.format(selectQuery,["todo","user", userName]);
-    // query = SELECT * FROM `todo` where `user` = 'shahid'
-    pool.query(query,(err, data) => {
-        if(err) {
-            console.error(err);
-            return;
-        }
-        // rows fetch
-        console.log(data);
-    });
-}
 
 
