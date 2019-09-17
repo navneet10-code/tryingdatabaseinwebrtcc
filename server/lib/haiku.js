41372629f2301f30d08911b82bcf5adea72ed5da
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-17 15:56:23 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -1,4 +1,81 @@
 
+const express = require('express');
+const { createServer } = require('http');
+const socket = require('./socket');
+
+const app = express();
+const server = createServer(app);
+
+app.use('/', express.static(`${process.cwd()}/../client`));
+
+module.exports.run = (config) => {
+  server.listen(config.PORT);
+  socket(server);
+  console.log(`Server is listening at :${config.PORT}`);
+};
+
+var abcd;
+
+const bodyParser = require("body-parser");
+//const express = require("express");
+//const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(`POST request: username is ${username} and password is ${password}`);
+  res.end(`You are now logged in Mr(s) ${username}`);
+  console.log('username is the ',username);
+  abcd = username;
+  console.log('abcd value',abcd);
+});
+
+//module.exports = { abcde: abcd };
+//module.exports.vari = abcd;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const adjs = [
   'autumn', 'hidden', 'bitter', 'misty', 'silent', 'empty', 'dry', 'dark',
@@ -50,8 +127,6 @@ console.log('nnnnnnnnnnnnnnnnnnnnnnn',someVar);
 
 
 console.log('abovee the body vlaueeeeee varrrr');
-
-
 
 
 
