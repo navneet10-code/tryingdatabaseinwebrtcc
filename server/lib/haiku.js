7bfcfcb199f0385b22edd001ccb6aaa4b64751dc
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-17 16:49:58 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -3,6 +3,54 @@ const express = require('express');
 const { createServer } = require('http');
 const socket = require('./socket');
 
+
+
+
+
+
+var mysql = require('mysql'); 
+var con = mysql.createConnection({ 
+host : 'sql12.freemysqlhosting.net',
+database : 'sql12304794',
+user : 'sql12304794',
+password : 'PLSEEGHnWv', 
+}); 
+con.connect(function(err) { 
+if (err) throw err; 
+con.query("SELECT UserName FROM user where email='parth@gmail.com' limit 1 ", function (err, result) { 
+if (err) throw err; 
+console.log(result); 
+setValue(result);
+
+}); 
+}); 
+
+function setValue(value) {
+someVar = value[0].UserName;
+console.log('nnnnnnnnnnnnnnnnnnnnnnn',someVar);
+}
+
+
+console.log('abovee the body vlaueeeeee varrrr');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const app = express();
 const server = createServer(app);
 
@@ -103,30 +151,6 @@ const nouns = [
 
 
 
-var mysql = require('mysql'); 
-var con = mysql.createConnection({ 
-host : 'sql12.freemysqlhosting.net',
-database : 'sql12304794',
-user : 'sql12304794',
-password : 'PLSEEGHnWv', 
-}); 
-con.connect(function(err) { 
-if (err) throw err; 
-con.query("SELECT UserName FROM user where email='parth@gmail.com' limit 1 ", function (err, result) { 
-if (err) throw err; 
-console.log(result); 
-setValue(result);
-
-}); 
-}); 
-
-function setValue(value) {
-someVar = value[0].UserName;
-console.log('nnnnnnnnnnnnnnnnnnnnnnn',someVar);
-}
-
-
-console.log('abovee the body vlaueeeeee varrrr');
 
 
 
