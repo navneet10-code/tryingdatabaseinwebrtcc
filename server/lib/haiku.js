6892b5c8287e5c4761922a60cb27c080b5ddbc87
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-17 14:39:42 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -52,35 +52,6 @@ console.log('abovee the body vlaueeeeee varrrr');
 
 
 
-const bodyParser = require("body-parser");
-const express = require("express");
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-res.sendFile(`${__dirname}/index.html`);
-});
-
-app.post('/login', (req, res) => {
-const username = req.body.username;
-const password = req.body.password;
-console.log(`POST request: username is ${username} and password is ${password}`);
-res.end(`You are now logged in Mr(s) ${username}`);
-console.log('username is the ',username);
-});
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = () => {
