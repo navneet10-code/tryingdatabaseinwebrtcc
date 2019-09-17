6617705874d6df1cb8304823f2579e3687b74f43
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-17 18:15:00 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -1,5 +1,5 @@
 
-const express = require('express');
+/*const express = require('express');
 const { createServer } = require('http');
 const socket = require('./socket');
 
@@ -110,3 +110,4 @@ module.exports.run = (config) => {
   socket(server);
   console.log(`Server is listening at :${config.PORT}`);
 };
+*/
