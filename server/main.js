a3412b0bce15a988e17d05bd77e84a6cd7a596a6
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-17 13:50:33 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -28,21 +28,5 @@ app.listen(3000,function(){
   console.log("Started on PORT 3000");
 })*/
 
-const bodyParser = require("body-parser");
-const express = require("express");
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
-
-app.post('/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  console.log(`POST request: username is ${username} and password is ${password}`);
-  res.end(`You are now logged in Mr(s) ${username}`);
-});
 
 
